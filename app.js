cd63c770f1c06a887cd1b9acf7535f43bc5418e1
Juaniscarlos23jurez <104327530+Juaniscarlos23jurez@users.noreply.github.com>
2024-12-03 15:09:57 +0000

    Cambio de service

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 // app.js
+
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-      .register('/service-worker.js')
-      .then((registration) => {
-        console.log('Service Worker registrado con éxito:', registration);
-      })
-      .catch((error) => {
-        console.log('Error al registrar el Service Worker:', error);
-      });
-  }
-  
+  navigator.serviceWorker
+    .register('/service-worker.js')  // Asegúrate de que la ruta sea la correcta
+    .then((registration) => {
+      console.log('Service Worker registrado correctamente', registration);
+    })
+    .catch((error) => {
+      console.log('Error al registrar el Service Worker:', error);
+    });
+}
--- service-worker.js
+++ service-worker.js
@@ -1,39 +1,37 @@
 // service-worker.js
 
-// Asegúrate de que Workbox esté cargado
 if (workbox) {
-    console.log('Workbox está cargado');
+  console.log('Workbox está cargado');
   
-    // Cache de los archivos estáticos, por ejemplo, imágenes, CSS y JS
-    workbox.routing.registerRoute(
-      ({ request }) => request.destination === 'image',
-      new workbox.strategies.CacheFirst({
-        cacheName: 'images-cache',
-        plugins: [
-          new workbox.expiration.Plugin({
-            maxEntries: 10,
-            maxAgeSeconds: 24 * 60 * 60, // 1 día
-          }),
-        ],
-      })
-    );
-  
-    workbox.routing.registerRoute(
-      ({ request }) =>
-        request.destination === 'script' || request.destination === 'style',
-      new workbox.strategies.StaleWhileRevalidate({
-        cacheName: 'static-resources-cache',
-      })
-    );
-  
-    // Para HTML y otros recursos estáticos
-    workbox.routing.registerRoute(
-      ({ url }) => url.origin === self.location.origin,
-      new workbox.strategies.CacheFirst({
-        cacheName: 'html-cache',
-      })
-    );
-  } else {
-    console.log('Workbox no está disponible');
-  }
-  
+  // Cache de los archivos estáticos, por ejemplo, imágenes, CSS y JS
+  workbox.routing.registerRoute(
+    ({ request }) => request.destination === 'image',
+    new workbox.strategies.CacheFirst({
+      cacheName: 'images-cache',
+      plugins: [
+        new workbox.expiration.Plugin({
+          maxEntries: 10,
+          maxAgeSeconds: 24 * 60 * 60, // 1 día
+        }),
+      ],
+    })
+  );
+
+  workbox.routing.registerRoute(
+    ({ request }) =>
+      request.destination === 'script' || request.destination === 'style',
+    new workbox.strategies.StaleWhileRevalidate({
+      cacheName: 'static-resources-cache',
+    })
+  );
+
+  // Para HTML y otros recursos estáticos
+  workbox.routing.registerRoute(
+    ({ url }) => url.origin === self.location.origin,
+    new workbox.strategies.CacheFirst({
+      cacheName: 'html-cache',
+    })
+  );
+} else {
+  console.log('Workbox no está disponible');
+}
